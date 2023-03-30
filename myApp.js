f37c642efbee4aa9e0c08fbc04a68abaa76fa3e2
Jay <jaykapadia389@gmail.com>
2023-03-30 21:53:33 +0530

    error

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
@@ -68,3 +70,5 @@ exports.createManyPeople = createManyPeople;
 exports.removeById = removeById;
 exports.removeManyPeople = removeManyPeople;
 exports.queryChain = queryChain;
+
+
